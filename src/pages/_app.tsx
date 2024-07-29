68ea6d7cf345111195bb4e3160add2f79b401c65
shakthi.m <shakthi.m@go-mmt.com>
2024-07-29 20:32:47 +0530

    random changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
+import React from "react";
+import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import {ThemeProvider, ToastProvider, useTheme} from 'shadster-ui' ;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+
+  return (
+
+      <ThemeProvider>
+        <ToastProvider>
+          <Component {...pageProps} />
+        </ToastProvider>
+      </ThemeProvider>
+  );}
